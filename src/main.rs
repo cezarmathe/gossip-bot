16cecae04b0026bfebcc2bb0f086f65595a2d74c
cezarmathe <me@cezarmathe.com>
2020-08-20 10:03:03 +0300

    Implement the `main` module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate anyhow;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+#[macro_use]
+extern crate thiserror;
+
+mod commands;
+mod discord;
+mod service;
+mod storage;
+
+use std::env;
+
+fn main() -> anyhow::Result<()> {
+    kankyo::load()?;
+    env_logger::init();
+
+    let token = env::var("GOSSIP_BOT_TOKEN")?;
+    let prefix = env::var("GOSSIP_BOT_PREFIX")?;
+
+    {
+        let _ = storage::DatabaseWrapper::get();
+    }
+
+    discord::run(&token, &prefix)
 }
